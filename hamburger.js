af8d8748f0a04d7104c84b9e08b4ed160d004ea8
safa <ahmedsafa0759@gmail.com>
2024-01-26 02:16:04 +0600

    lab2 topics added|responsive

---
+++ hamburger.js
@@ -0,0 +1,10 @@
+function showHam()
+{
+    const side_bar = document.querySelector('.side_bar')
+    side_bar.style.display = 'flex'
+}
+function hideSidebar()
+{
+    const side_bar = document.querySelector('.side_bar')
+    side_bar.style.display = 'none'
+}
