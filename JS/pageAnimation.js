bed0e42bdac64d216727e91016662cf84775d83c
safa <ahmedsafa0759@gmail.com>
2024-01-27 00:57:50 +0600

    about is responsive

---
+++ JS/pageAnimation.js
@@ -0,0 +1,27 @@
+$('.btn').on('click', function (event) {
+    if (this.hash != '') {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800
+        );
+    }
+});
+
+// for navbar links
+
+$('.btnnavbars').on('click', function (event) {
+    if (this.hash != '') {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800
+        );
+    }
+});
